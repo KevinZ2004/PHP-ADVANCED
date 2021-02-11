137aba2d7d71a23f6019cac6b21b370295db64ff
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-02-11 11:17:46 +0100

    1- arrays - taak 01 af

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,24 @@
+<?php
+$lengte['Piet'] = 187;
+$lengte['Muhammad'] = 184;
+$lengte['Els'] = 168;
+
+$achternaam['Piet'] = "pieters";
+$achternaam['Muhammad'] = "Ali";
+$achternaam['Els'] = "Boom";
+
+$voornaam['Piet'] = "Piet";
+$voornaam['Muhammad'] = "Muhammad";
+$voornaam['Els'] = "Els";
+
+$leeftijd['Piet'] = 18;
+$leeftijd['Muhammad'] = 17;
+$leeftijd['Els'] = 19;
+
+$klas['Piet'] = "9A";
+$klas['Muhammad'] = "9G";
+$klas['Els'] = "9B";
+
+echo "De lengte van " . "$voornaam[Muhammad] ". "$achternaam[Muhammad] ". "is ". $lengte['Muhammad']  . " centimeter " . "hij is ". "$leeftijd[Muhammad]". " jaar oud". " en zit in klas".  " $klas[Muhammad].";
+
+?>  

ab18ac8c1f97cc16263d7336007afa4dbdf58555
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-03-04 15:26:08 +0100

    de helft van de eerste opdracht

---
+++ opdracht/PHP-eindopdracht/advanced-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie-edge">
+  <title>Form</title>
+</head>
+<body>
+  <form action="results.html" method="GET">
+    <div>
+    <label for="">Tabelranddickte (px):</label>
+    <input name="Tabelranddickte (px):">
+    </div>
+    <div>
+    <label>cel-padding (px)</label>
+    <input name="cel-padding (px)">
+    </div>
+    <button>Submit</button>
+
+  </form>
+
+
+</html>

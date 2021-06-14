9e07713e1894e590f9e7250908dd745c559a0297
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-06-15 00:25:47 +0200

    opdracht 1 af

--- opdracht/PHP-eindopdracht/advanced-1.php
+++ opdracht/PHP-eindopdracht/advanced-1.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="advanced-2.php">
+        <link rel="stylesheet" href="style2.css">
     </head>
     <body>
         <div class="wrapper">
@@ -17,15 +17,26 @@
             </select>
             <a>achtergrond kleur</a>
 
+            <select name="tekst-kleur" id="tekst-kleur">
+                <?php
+                    $kleuren = array("red", "blue", "green", "black", "brown");
+                        foreach($kleuren as $optie){
+                        echo "<option value=$optie>$optie</option>";};
+                ?>
+            </select>
+            <a>tekst kleur</a>
+
             <input type="text" name="border">
             <a>border</a>
 
             <input type="text" name="padding">
             <a>padding</a>
 
-            <input type="submit" name="submit" value="verstuur">
+            <input type="submit" name="submit" value="vesrtuur">
 
-             
-         </div>
+
+
+        </div>
     </body>
+
 </html>

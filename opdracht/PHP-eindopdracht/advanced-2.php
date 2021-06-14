689f84664941eb3881274dd6e53754406eaf9178
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-06-15 00:29:14 +0200

    verder gewerkt aan PHP

--- opdracht/PHP-eindopdracht/advanced-2.php
+++ opdracht/PHP-eindopdracht/advanced-2.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
         <link rel="stylesheet" href="style.css">
-        <style> 
-             body {
-                 background-color: <?php echo $_POST["achtergrond-kleur"] ?>;
-                 color: <?php echo $_POST["tekst-kleur"] ?>;
-             }
-                 table,tr,td {
-                     padding: <?php echo $_POST["padding"] ?>px;
-                     border: <?php echo $_POST["border"] ?>px black;
-                 }
+        <style>
+                body {
+                    background-color: <?php echo $_POST["achtergrond-kleur"] ?> ;
+                    color: <?php echo $_POST["tekst-kleur"] ?>;
+                }
+                table,tr,td {
+                    padding: <?php echo $_POST["padding"] ?>px;
+                    border: <?php echo $_POST["border"] ?>px solid black;
+                }
         </style>
     </head>
     <body>
@@ -18,25 +18,26 @@
             <div class="flex-container">
                 <table>
                     <tr>
-                       <th>key</th>
-                       <th>value</th>
+                        <th>key</th>
+                        <th>value</th>
                     </tr>
 
                     <?php
                         $info2= array("naam"=>"naam", "achternaam"=>"achternaam", "klas"=>"klas", "muziek"=>"favoriete muziek");
-                        $info= array("naam=>Kevin", "achternaam"=>"Zwolschen", "klas"=>"LCTAOO0d", "muziek"=>"pop");
+                        $info= array("naam"=>"Kevin", "achternaam"=>"Zwolschen", "klas"=>"LCTAOOoD", "muziek"=>"pop muziek");
+                        
 
+                        function maakrij($info2){
 
-                        function MaakRij($info2){
-
-                                 return $info2;
+                                return $info2;
                         }
-                             foreach($info2 as $index => $thing){
+                            foreach($info2 as $index => $thing){
 
-                                 echo "<tr><td>$thing</td> $info[$index] </td></tr>";
-                             }
+                                echo "<tr><td> $thing </td><td> $info[$index] </td></tr>";
+                            }
 
                     ?>
+                    
                 </table>
             </div>
         </div>

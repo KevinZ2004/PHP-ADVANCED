f53f6d9930e79ea8ea2c302a07916043c99de7ba
KevinZ2004 <70368913+KevinZ2004@users.noreply.github.com>
2021-06-22 10:43:07 +0200

    opdracht 2 af

--- opdracht/PHP-eindopdracht/advanced-1.php
+++ opdracht/PHP-eindopdracht/advanced-1.php
@@ -8,15 +8,18 @@
 
             <form name="invoer" action="advanced-2.php" method="POST">
 
-            <select name="achtergrond-kleur" id="achtergrond-kleur">
+
+            <a>achtergrondkleur:</a>
+            <select name="achtergrondkleur" id="achtergrondkleur">
                 <?php
                     $kleuren = array("red", "blue", "green", "black", "brown");
                         foreach($kleuren as $optie){
                         echo "<option value=$optie>$optie</option>";};
                 ?>
             </select>
-            <a>achtergrond kleur</a>
+            
 
+            <a>tekstkleur:</a>
             <select name="tekst-kleur" id="tekst-kleur">
                 <?php
                     $kleuren = array("red", "blue", "green", "black", "brown");
@@ -24,19 +27,18 @@
                         echo "<option value=$optie>$optie</option>";};
                 ?>
             </select>
-            <a>tekst kleur</a>
 
+
+            <a>border:</a>
             <input type="text" name="border">
-            <a>border</a>
 
+            <a>padding:</a>
             <input type="text" name="padding">
-            <a>padding</a>
 
             <input type="submit" name="submit" value="verstuur">
-
-
 
         </div>
     </body>
 
 </html>
+
